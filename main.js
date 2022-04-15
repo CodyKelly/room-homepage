02c69e92600485d8e1feb1a8fd7eee89a4feed0b
CodyKelly <codykellydesign@gmail.com>
2022-04-15 11:36:36 -0600

    Slide containers now work independently of each other

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const STATES = {
-  animationActive: 'animationActive',
-  idle: 'idle',
-};
+animationActive = false;
 
-let state = STATES.idle;
+// HTML slide containers
+let slideContainers = document.getElementsByClassName('slide--container');
+slideContainers = Array.from(slideContainers);
 
-let slides = document.getElementsByClassName('slide');
-let currentSlideIndex = 0;
+// Array of objects containing data for each individual slide container
+let containers = [];
+
+// Create a new object for each slide container on the page
+for (let i = 0; i < slideContainers.length; i++) {
+  let children = slideContainers[i].children;
+  children = Array.from(children);
+  containers.push({
+    currentSlideIndex: 0,
+    slides: children.filter((child) => {
+      // Get only children that are slides
+      return child.classList.contains('slide');
+    }),
+  });
+}
 
 // Get index of the slide to the left of the given index
-function getLeftIndex(slideIndex) {
+function getLeftIndex(slideIndex, slides) {
   if (slideIndex === 0) {
     return slides.length - 1;
   }
@@ -18,7 +30,7 @@ function getLeftIndex(slideIndex) {
 }
 
 // Get index of the slide to the right of the given index
-function getRightIndex(slideIndex) {
+function getRightIndex(slideIndex, slides) {
   if (slideIndex === slides.length - 1) {
     return 0;
   }
@@ -27,55 +39,70 @@ function getRightIndex(slideIndex) {
 }
 
 function slideLeft() {
-  if (state !== STATES.idle) {
-    return;
+  // Slides the slides of all slide containers to the left
+
+  if (animationActive) {
+    return; // We don't want to start a new animation if one's already active.
   }
 
-  let currentSlide = slides[currentSlideIndex];
-  let nextSlideIndex = getRightIndex(currentSlideIndex);
-  let nextSlide = slides[nextSlideIndex]; // Slide to the right of the current slide
+  for (let i = 0; i < containers.length; i++) {
+    let { currentSlideIndex, slides } = containers[i];
 
-  state = STATES.animationActive; // Disable slides while animation is playing
+    let currentSlide = slides[currentSlideIndex];
+    let nextSlideIndex = getRightIndex(currentSlideIndex, slides);
+    let nextSlide = slides[nextSlideIndex]; // Slide to the right of the current slide
 
-  currentSlide.classList.add('slide-out-left'); // currentSlide slides out of frame
-  nextSlide.classList.remove('inactive');
-  nextSlide.classList.add('slide-in-left'); // And the slide to its right slides into frame
+    animationActive = true; // Disable slides while animation is playing
 
-  let onAnimationEnd = () => {
-    currentSlide.classList.remove('slide-out-left');
-    currentSlide.classList.add('inactive');
-    nextSlide.classList.remove('slide-in-left');
-    currentSlideIndex = getRightIndex(currentSlideIndex);
-    state = STATES.idle;
-    currentSlide.removeEventListener('animationend', onAnimationEnd);
-  };
+    currentSlide.classList.add('slide-out-left'); // currentSlide slides out of frame
+    nextSlide.classList.remove('inactive');
+    nextSlide.classList.add('slide-in-left'); // And the slide to its right slides into frame
 
-  currentSlide.addEventListener('animationend', onAnimationEnd);
+    let onAnimationEnd = () => {
+      currentSlide.classList.remove('slide-out-left');
+      currentSlide.classList.add('inactive');
+      nextSlide.classList.remove('slide-in-left');
+      containers[i].currentSlideIndex = getRightIndex(
+        currentSlideIndex,
+        slides,
+      );
+      animationActive = false;
+      currentSlide.removeEventListener('animationend', onAnimationEnd);
+    };
+
+    currentSlide.addEventListener('animationend', onAnimationEnd);
+  }
 }
 
 function slideRight() {
-  if (state !== STATES.idle) {
-    return;
+  // Slides the slides of all slide containers to the right
+
+  if (animationActive) {
+    return; // We don't want to start a new animation if one's already active.
   }
 
-  let currentSlide = slides[currentSlideIndex];
-  let nextSlideIndex = getLeftIndex(currentSlideIndex);
-  let nextSlide = slides[nextSlideIndex]; // Slide to the left of the current slide
+  for (let i = 0; i < containers.length; i++) {
+    let { currentSlideIndex, slides } = containers[i];
 
-  state = STATES.animationActive; // Disable slides while animation is playing
+    let currentSlide = slides[currentSlideIndex];
+    let nextSlideIndex = getLeftIndex(currentSlideIndex, slides);
+    let nextSlide = slides[nextSlideIndex]; // Slide to the left of the current slide
 
-  currentSlide.classList.add('slide-out-right'); // currentSlide slides out of frame
-  nextSlide.classList.remove('inactive');
-  nextSlide.classList.add('slide-in-right'); // And the slide to its left slides into frame
+    animationActive = true; // Disable slides while animation is playing
 
-  let onAnimationEnd = () => {
-    currentSlide.classList.remove('slide-out-right');
-    currentSlide.classList.add('inactive');
-    nextSlide.classList.remove('slide-in-right');
-    currentSlideIndex = getLeftIndex(currentSlideIndex);
-    state = STATES.idle;
-    currentSlide.removeEventListener('animationend', onAnimationEnd);
-  };
+    currentSlide.classList.add('slide-out-right'); // currentSlide slides out of frame
+    nextSlide.classList.remove('inactive');
+    nextSlide.classList.add('slide-in-right'); // And the slide to its right slides into frame
 
-  currentSlide.addEventListener('animationend', onAnimationEnd);
+    let onAnimationEnd = () => {
+      currentSlide.classList.remove('slide-out-right');
+      currentSlide.classList.add('inactive');
+      nextSlide.classList.remove('slide-in-right');
+      containers[i].currentSlideIndex = getLeftIndex(currentSlideIndex, slides);
+      animationActive = false;
+      currentSlide.removeEventListener('animationend', onAnimationEnd);
+    };
+
+    currentSlide.addEventListener('animationend', onAnimationEnd);
+  }
 }
